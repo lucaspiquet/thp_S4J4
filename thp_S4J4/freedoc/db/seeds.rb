323ef52e56a1e3b2b5e3e2c75d45373c83682389
lucaspiquet <piquet.lucas@gmail.com>
2019-04-25 18:54:19 +0200

    exo 1

--- thp_S4J4/freedoc/db/seeds.rb
+++ thp_S4J4/freedoc/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Job.title, zip_code: Faker::Code.npi)
+end
+
+10.times do |index|
+  patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::FunnyName.two_word_name)
+end
+
+10.times do |index|
+  appointment = Appointment.create!(date: Faker::Date.forward(45), patient: Patient.all.sample, doctor: Doctor.all.sample)
+end
